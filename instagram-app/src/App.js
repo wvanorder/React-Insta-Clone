bcfd47d6ad71a0cb6e8d49308f0fe47058680cbf
VanOrder <vanorder@VanOrders-iMac.local>
2019-06-05 14:45:49 -0800

    made working log-in page and an array of users to pass tests for log in authentication

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,118 +1,29 @@
 import React from 'react';
 import './App.css';
-import SearchBar from './components/SearchBar/SearchBar';
-import dummyData from './dummy-data';
-import PostContainer from './components/PostContainer/PostContainer';
+import PostsPage from './components/PostContainer/PostsPage';
+import withAuthenticate from './components/authentication/withAuthenticate';
+import Login from './components/Login/Login';
+
+
+const ComponentFromWithAuthenticate = withAuthenticate(PostsPage)(Login);
 
 class App extends React.Component {
-
-  constructor() {
+  constructor(){
     super();
-    this.state = {
-      data: [],
-      search: '',
-      newComment: '',
-      error: null,
-    };
   }
 
   
-  componentDidMount() {
-    if(!localStorage.getItem('data')){
-      setTimeout( () =>
-      this.setState({
-        data: dummyData
-      }), 2000)
-    } else{
-      this.setState({
-        data: JSON.parse(localStorage.getItem('data')),
-        isLoading: false
-      })
-    }
-    
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if(prevState.data !== this.state.data) {
-      localStorage.setItem('data', JSON.stringify(this.state.data));
-      localStorage.setItem('dataDate', Date.now());
-    }
-  }
-
-
-
-  searchPosts = (value = this.state.search) => {
-    const newArr = this.state.data.map(post => {
-      if(post.username.toLowerCase().includes(value.toLowerCase())) {
-        post.match = true;
-        return post;
-      }
-      post.match = false;
-      return post;
-    });
-    this.setState({
-      data: newArr
-    });
-  }
-
-  searchInput = event => {
-    this.setState({
-        [event.target.name]: event.target.value,
-    });
-};
-
-
-addComment = (id) => {
-  if(this.state.newComment === '') {
-      return this.setState({
-          error: 'You must type something to leave a comment!',
-      });
-  }
-  const newComment = {
-      username: 'anonymous',
-      text: this.state.newComment,
-      
-  };
-  const posts = JSON.parse(localStorage.getItem('data')).map(post => {
-    if(post.id === id) { 
-      newComment.id = post.comments.length + 1;
-      post.comments = [...post.comments, newComment]
-    } 
-    return post;
-  })
-  this.setState({
-      data: [...posts],
-      newComment: '',
-      error: null,
-  })
-} 
-
-commentInput = event => {
-  this.setState({
-      newComment: event.target.value,
-  });
-};
-
-
 
     
  render() {
-   return (
-    <div className="App">
-    <SearchBar searchTerm={this.state.search} inputChange={this.searchInput}/>
-    <PostContainer 
-    posts={this.state.data.filter(post => {
-      return post.username.toLowerCase().includes(this.state.search.toLowerCase())
-    })} 
-    searchTerm={this.state.search} 
-    newComment={this.state.newComment}
-    error={this.state.error}
-    addComment={this.addComment}
-    commentInput={this.commentInput}
-    />
-  </div>
-   )
-     
-  } 
+    return (
+    <div>
+      <ComponentFromWithAuthenticate />
+    </div>
+    
+    
+    )
+    };
    
   
 }
--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './searchBar.scss';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faCompass, faHeart, faUser} from '@fortawesome/free-regular-svg-icons';
+import {Button} from 'antd';
 
 const SearchBar = props => {
 
@@ -11,6 +12,7 @@ const SearchBar = props => {
             <img className='logo' src='https://online-harvest.com/en/wp-content/uploads/2018/09/if_38-instagram_1161953-1-e1538728533467.png'/>
             <img className='text-logo' src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1280px-Instagram_logo.svg.png' />
             <input className='search' placeholder='search' name='search' value={props.searchTerm} onChange={props.inputChange}/>
+            <Button type="primary" htmlType="submit" className="login-form-button" onClick={props.logOut}>Log Out</Button>
             <div className='icons'>
                 <FontAwesomeIcon className='icon' icon={faCompass} />
                 <FontAwesomeIcon className='icon' icon={faHeart} />
